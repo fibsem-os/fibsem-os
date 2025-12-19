09b450a7c364caaadbc76710ba97d2f51c15285f
Patrick Cleeve <patrick@openfibsem.org>
2025-12-19 18:55:16 +1100

    [refactor] clean up basic milling task

--- fibsem/applications/autolamella/workflows/tasks/tasks.py
+++ fibsem/applications/autolamella/workflows/tasks/tasks.py
@@ -1149,18 +1149,20 @@ class BasicMillingTask(AutoLamellaTask):
     def _run(self) -> None:
         """Run the basic milling task."""
 
+        image_settings = self.config.imaging
+        image_settings.path = self.lamella.path
+
         self.log_status_message("MOVE_TO_LAMELLA", "Moving to Lamella Position...")
         self.microscope.safe_absolute_stage_movement(self.lamella.stage_position)
 
         self.log_status_message("RUN_MILLING", "Milling...")
 
         for key, milling_task_config in self.config.milling.items():
+            milling_task_config.acquisition.imaging.path = self.lamella.path
             milling_task_config = self.update_milling_config_ui( milling_task_config)
             self.config.milling[key] = deepcopy(milling_task_config)
 
         self.log_status_message("ACQUIRE_REFERENCE_IMAGES", "Acquiring Reference Images...")
-        image_settings = ImageSettings()
-        image_settings.path = self.lamella.path
         self._acquire_set_of_reference_images(image_settings)
 
 
