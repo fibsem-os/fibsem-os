6855e5366fed35d9616f7d4604b0bf02858e5880
Patrick Cleeve <patrick@openfibsem.org>
2026-01-20 10:51:53 +1100

    [refactor] increase default fov for default protocol

--- fibsem/structures.py
+++ fibsem/structures.py
@@ -2336,7 +2336,7 @@ class RangeLimit:
 @dataclass
 class ReferenceImageParameters:
     imaging: ImageSettings = field(default_factory=ImageSettings)
-    field_of_view1: float = field(default=80e-6, metadata={"tooltip": "Field of view for first reference image"})
+    field_of_view1: float = field(default=120e-6, metadata={"tooltip": "Field of view for first reference image"})
     field_of_view2: float = field(default=150e-6, metadata={"tooltip": "Field of view for second reference image"})
     acquire_sem: bool = field(default=True, metadata={"tooltip": "Whether to acquire SEM reference images"})
     acquire_fib: bool = field(default=True, metadata={"tooltip": "Whether to acquire FIB reference images"})
@@ -2359,7 +2359,7 @@ class ReferenceImageParameters:
         imaging = ImageSettings.from_dict(settings.get("imaging", {}))
         return ReferenceImageParameters(
             imaging=imaging,
-            field_of_view1=settings.get("field_of_view1", 80e-6),
+            field_of_view1=settings.get("field_of_view1", 120e-6),
             field_of_view2=settings.get("field_of_view2", 150e-6),
             acquire_sem=settings.get("acquire_sem", True),
             acquire_fib=settings.get("acquire_fib", True),
